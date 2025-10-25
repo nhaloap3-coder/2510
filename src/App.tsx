efea5977b5cf5dcf4199124743cb3e8a378ca1fb
nhaloap3-coder <nhaloap3@gmail.com>
2025-10-25 16:33:15 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import "./styles.css";
+// @ts-ignore
+import Home from "./Home";
+// @ts-ignore
+import Layout from "./Layout";
+// @ts-ignore
+import Trang1 from "./Trang1";
+// @ts-ignore
+import Chitietsanpham from "./Chitietsanpham";
+
+// @ts-ignore
+import ListProducts from "./ListProducts";
+
+// @ts-ignore
+import Trang2 from "./Trang2";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  // return <Layout />;
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<ListProducts />} />
+          <Route path="trang1" element={<Trang1 />} />
+          <Route path="sanpham/:id" element={<Chitietsanpham />} />
+          <Route path="trang2" element={<Trang2 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
